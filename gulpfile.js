5f1410d166404a6afbe0650b35b0c5ae257b6b59
Sławomir Kulig <slawomir.kulig@comarch.pl>
2017-02-17 16:44:52 +0100

    prompt

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,93 @@
 var gulp = require('gulp');
-var gutil = require('gulp-util')
-var duration = require('gulp-duration')
-
+var gutil = require('gulp-util');
+var duration = require('gulp-duration');
 var dirSync = require( 'gulp-directory-sync' );
+var prompt = require( 'gulp-prompt' );
+var fs = require('fs');
 
-var corpoSrc =  'C://Users//J-LABS//code//bnp_cib//wars//fortis-corpo-cib-web//src//main//webapp//'
-var corpoDest = 'C://IBM//WebSphere//AppServer//profiles//fbpl//installedApps//JLABS-5LC7K32Cell01//fortis-corpo-cib-app.ear//fortis-corpo-cib-web-2.1.war//';
+var src = {
+	corpo: '//wars//fortis-corpo-cib-web//src//main//webapp//',
+	retail: '//wars//fortis-retail-cib-web//src//main//webapp//',
+	demo: '//wars//fortis-login-cib-web//src//main//webapp//ver//'
+};
 
-var adkSrc =  	'C://Users//J-LABS//code//bnp_cib//wars//fortis-corpo-adk-web//src//main//webapp//';
-var adkDest = 	'C://IBM//WebSphere//AppServer//profiles//fbpl//installedApps//JLABS-5LC7K32Cell01//fortis-corpo-adk-web-2.1_war.ear//fortis-corpo-adk-web-2.1.war//';
+var dest = {
+	corpo: '//fortis-corpo-cib-app.ear//fortis-corpo-cib-web-2.1.war//',
+	retail: '//fortis-retail-cib-app.ear//fortis-retail-cib-web-2.1.war//',
+	demo: '//fortis-retail-cib-app.ear//fortis-login-cib-web-2.1.war//ver//'
+};
 
+var run = {};
 
-gulp.task( 'adk', function() {
-    return gulp.src( '' )
-        .pipe(dirSync( adkSrc, adkDest, { printSummary: true, nodelete: true } ))
-        .on('error', gutil.log);
+gulp.task('watch', function() {
+
+	run = require('./config.json');
+
+	setInterval(function() {
+		gulp.start('sync');
+	}, 5000)
+});
+
+gulp.task( 'sync', function() {
+	return gulp.src( 'config.json' )
+		.pipe(dirSync( run.demoSrc, run.demoDest, { printSummary: true, nodelete: true } ))
+		.pipe(dirSync( run.retailSrc, run.retailDest, { printSummary: true, nodelete: true } ))
+		.pipe(dirSync( run.corpoSrc, run.corpoDest, { printSummary: true, nodelete: true } ))
+		.on('error', gutil.log);
 } );
 
-gulp.task( 'corpo', function() {
-    return gulp.src( '' )
-        .pipe(dirSync( corpoSrc, corpoDest, { printSummary: true, nodelete: true } ))
-        .on('error', gutil.log);
-} );
 
+function checkIfDirectoryExists(directory) {
+	try {
+		fs.statSync(directory);
+		return true;
+	} catch(e) {
+		gutil.log('\nLocation ' + directory + ' not exists\n Please enter directory again');
+		return false;
+	}
+}
 
-gulp.task('sync', function() {
-  setInterval(function() {
-		gulp.start('adk');
-  }, 5000)
+function gluePaths(config){
+
+	var os = require('os');
+
+	config.demoSrc = config.project + src.demo;
+	config.demoDest = config.websphere + '//AppServer//profiles//fbpl//installedApps//' + os.hostname() + 'Cell01' + dest.demo;
+
+	config.retailSrc = config.project + src.retail;
+	config.retailDest = config.websphere + '//AppServer//profiles//fbpl//installedApps//' + os.hostname() + 'Cell01' + dest.retail;
+
+	config.corpoSrc = config.project + src.corpo;
+	config.corpoDest = config.websphere + '//AppServer//profiles//fbpl//installedApps//' + os.hostname() + 'Cell01' + dest.corpo;
+}
+
+gulp.task('configure', function() {
+	return gulp.src('')
+		.pipe(prompt.prompt([
+			{
+				type: 'input',
+				name: 'websphere',
+				message: 'What is your Websphere directory? (example - C://IBM//WebSphere)',
+				validate: function(websphere){
+					return checkIfDirectoryExists(websphere);
+				}
+			},
+			{
+				type: 'input',
+				name: 'project',
+				message: 'What is your project location? (example - C://Users//J-LABS//Projects//bnp_cib)',
+				validate: function(websphere){
+					return checkIfDirectoryExists(websphere);
+				}
+			}
+		], function(config){
+			gutil.log('Check config.json if everything is ok and run gulp again!');
+			gluePaths(config);
+			fs.writeFileSync('config.json', JSON.stringify(config));
+		}));
+});
+
+gulp.task('default', function() {
+	var configurationFile = './config.json';
+	fs.existsSync(configurationFile) ? gulp.start('watch') : gulp.start('configure');
 });
